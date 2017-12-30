5ce5357ed46ecf0d3bfb6ea673f4fb840747bda1
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-12-30 16:18:21 +0100

    Left and Right Paddle Collisions

--- ball.js
+++ ball.js
@@ -1,4 +1,6 @@
+
 function Ball(x,y,mx,my,radius){
+    
 this.x = x
 this.y = y
 this.mx = mx
@@ -12,17 +14,12 @@ this.radius = radius
         ctx.closePath()
     }
 
-    this.update = function(){
-        
-        if(this.x + this.radius > canvas.width || this.x < 0 + this.radius){
-            this.mx = -this.mx
-        }
-        if(this.y + this.radius > canvas.height || this.y < 0 + this.radius){
-            this.my = -this.my
-        }
     
-       
-        
+    this.update = function(){
+        if(this.y + this.radius > canvas.height || this.y < 0 + this.radius){
+                this.my = -this.my
+    }
+    
         this.x += this.mx
         this.y += this.my
         this.draw()
--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var mouse = {
     x: null,
     y: null
 }
+
 document.addEventListener('keydown',function(event){
     if(event.keyCode == 40){
         up = true
@@ -35,7 +36,17 @@ document.addEventListener('keyup',function(event){
 canvas.width = 800
 canvas.height = 600
 
+//GAME OVER Function
 
+function GameOver(){
+    ctx.font = "30px Arial";
+    ctx.fillText("Game Over",canvas.width/2 - 70,canvas.height/2);
+    ctx.fillStyle = 'white'
+    setTimeout(()=>{ 
+    this.x = canvas.width /2
+    this.y = canvas.height /2
+    }, 4000)
+}
 //Calculate Position of the mouse
     function calculateMousePos(event){
         //Measure Size of our canvas
@@ -60,7 +71,9 @@ function CanvasProps(){
 
 var pdlLeft = new paddleLeft(0, (canvas.height/2)-80/2, 10, paddleHeight, 10)
 var pdlRight = new paddleRight(canvas.width - 10,0, 10, paddleHeight, 10)
-var ball = new Ball(canvas.width/2, canvas.height/2, 5, 5, 10)
+var ball = new Ball(canvas.width/2, canvas.height/2, -5, 5, 10)
+
+
 function animate(){
     requestAnimationFrame(animate)
     ctx.clearRect(0,0, canvas.width,canvas.height)
@@ -68,5 +81,21 @@ function animate(){
     pdlLeft.update()
     ball.update()
     pdlRight.update()
+    if(ball.x < 0 + ball.radius){
+        if(ball.y  > pdlLeft.y  && ball.y < pdlLeft.y + paddleHeight){
+            ball.mx = -ball.mx
+    }else{
+        GameOver()
+    }
+    console.log(pdlRight.y);
+    
+    }
+    if(ball.x > canvas.width + ball.radius){
+        if(ball.y  > pdlRight.y  && ball.y < pdlRight.y + paddleHeight){
+            ball.mx = -ball.mx
+    }else{
+        GameOver()
+    }
+    }
 }
 animate()
